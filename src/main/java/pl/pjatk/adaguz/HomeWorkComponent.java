c66412fe82c298e83c9abe53ef1cd74a764986e3
Adam <mail2gzs@gmail.com>
2021-04-17 22:13:21 +0200

    Conditional

--- src/main/java/pl/pjatk/adaguz/HomeWorkComponent.java
+++ src/main/java/pl/pjatk/adaguz/HomeWorkComponent.java
@@ -1,12 +1,21 @@
 package pl.pjatk.adaguz;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-
+@ConditionalOnProperty(
+        value = "my.custom.boolean",
+        havingValue = "true"
+)
 public class HomeWorkComponent {
 
+
     public HomeWorkComponent(String result) {
-        //System.out.println(result);
+
+        System.out.println(result);
     }
+
 }
+
+
